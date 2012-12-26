d6342656a404735237066197712cc443885c192a
Ivan Vazquez <ivan@ivanvazquez.com>
2012-12-25 23:59:51 -0700

    vendors work

---
+++ setup-vendors.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+git submodule init && git submodule update
+
+### Submodule specific commands
+
+# emacs-w3m
+(cd vendors/emacs-w3m; autoconf && ./configure; make)
+
+# Pymacs, Rope, etc.
+
+(cd vendors/pymacs; make; sudo make install; emacs --batch -f batch-byte-compile pymacs.el)
+[ -x /usr/bin/easy_install ] || sudo apt-get install python-setuptools
+sudo easy_install \
+    "http://pypi.python.org/packages/source/r/rope/rope-0.9.4.tar.gz"
+sudo easy_install \
+    "http://pypi.python.org/packages/source/r/ropemacs/ropemacs-0.7.tar.gz"
+sudo easy_install \
+    "http://pypi.python.org/packages/source/r/ropemode/ropemode-0.2.tar.gz"
+
