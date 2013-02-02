cf42ee74612d27578c876b23d5e2b7eb216ac379
Ivan Vazquez <ivan@ivanvazquez.com>
2013-02-01 17:19:14 -0700

    added alternate font

--- iv-emacs/iv-arch-x.el
+++ iv-emacs/iv-arch-x.el
@@ -1,5 +1,8 @@
 ;;
 (cond ((eq window-system 'x)
-       (set-default-font "-misc-fixed-medium-r-normal--10-*-*"))
+       (cond ((> (x-display-pixel-width) 1280)
+              (set-default-font "-misc-fixed-medium-r-normal--13-*-*"))
+             (t 
+              (set-default-font "-misc-fixed-medium-r-normal--13-*-*"))))
       (t
        (error "Not x window system")))
